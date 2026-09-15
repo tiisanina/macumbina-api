b6528ba65f820b75a2eed3a303139ecf007e6fc1
tiisanina <gentilealessia85@gmail.com>
2026-09-15 13:24:30 +0200

    Update macumba.js

--- api/macumba.js
+++ api/macumba.js
@@ -16,8 +16,8 @@ export default function handler(req, res) {
     "se cucinerai qualcosa, lo brucerai mi dispiace...",
     "se questo mese non hai fatto la sub da Nj ti viene la febbre a 39, puoi rimediare entro oggi... se l'hai fatta la macumba oggi non ti colpirá",
     "Perdi tutti i game oggi, ahahhahahahha",
-    "Tuo padre si incazza con te oggi, guarda il lato positivo tu ce l'hai almeno..."
-    "perderà 4 ranked di fila"
+    "Tuo padre si incazza con te oggi, guarda il lato positivo tu ce l'hai almeno...",
+    "perderà 4 ranked di fila",
     "verrà inseguito/a da un cinghiale"
   ];
 
